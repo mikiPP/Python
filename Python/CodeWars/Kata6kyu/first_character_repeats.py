315f6c6277d9eb5abbb66bef9635fe1734a2445c
Miguel Angel Palao Palmer <miki.palao.palmer@gmail.com>
2019-01-03 19:04:07 +0100

    first_character_repeats añadido

---
+++ Python/CodeWars/Kata6kyu/first_character_repeats.py
@@ -0,0 +1,34 @@
+"""
+Find the first character that repeats in a String and return that character.
+
+first_dup('tweet') => 't'
+first_dup('like') => None
+This is not the same as finding the character that repeats first. In that case, an input of 'tweet' would yield 'e'.
+
+"""
+
+
+def first_duplicated(string):
+    letterFound = False
+    letterPosition = 0
+
+    for letter in string:
+        if string.count(letter) > 1 and letterFound == False:
+            letterPosition = string.find(letter)
+            letterFound = True
+
+    if letterFound:
+        return string[letterPosition]
+
+    return None
+
+
+if __name__ == '__main__':
+
+    assert first_duplicated('tweet') == 't'
+    assert first_duplicated('Ode to Joy') == ' '
+    assert first_duplicated('ode to joy') == 'o'
+    assert first_duplicated('bar') == None
+    assert first_duplicated('123123') == '1'
+    assert first_duplicated('!@#$!@#$') == '!'
+    assert first_duplicated('1a2b3a3c') == 'a'
